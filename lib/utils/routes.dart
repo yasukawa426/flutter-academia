7c89d8699a7389a896b2aa4ba5f0c4a835adc73e
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-08-10 22:32:30 -0300

    Rota de criação de linha

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/cupertino.dart';
+import 'package:projeto_academia/view/create/create_page.dart';
 import 'package:projeto_academia/view/home/home_page.dart';
 
 Map<String, Widget Function(dynamic)> routes = {
   '/' : (context) => HomePage(key: UniqueKey()),
+  '/create' : (context) => CreatePage(key: UniqueKey()),
 };
