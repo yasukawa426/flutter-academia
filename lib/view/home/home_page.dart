f6e1010a4926a35c42a3b81049b1242ac4cc2e7a
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-08-10 22:32:17 -0300

    Botão Adicionado

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Oiiiii"),
       ),
-      body: const Center(child: Text("Oie")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('create');
+                },
+                child: const Text("Adicionar corrida")),
+          )
+        ],
+      ),
     );
   }
 }
