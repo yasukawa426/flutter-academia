6da0cb1d0f6370768b758b01ca48a5f2f42ddb6b
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-08-12 15:53:23 -0300

    Import removido

--- lib/components/NavigationRail/custom_navigation_rail.dart
+++ lib/components/NavigationRail/custom_navigation_rail.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 
 class CustomNavigationRail extends StatefulWidget {
   const CustomNavigationRail({Key? key, required this.initialIndex})
